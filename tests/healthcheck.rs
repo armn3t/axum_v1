d834b7b3c9c75e85a73a9dea77907b1513ec5526
Armin <armhamos@gmail.com>
2024-04-07 01:22:38 +0300

    test: fix healthcheck test

--- tests/healthcheck.rs
+++ tests/healthcheck.rs
@@ -1,12 +1,13 @@
 use reqwest::{blocking::Client, StatusCode};
 
 pub mod common;
+use common::URL;
 
 #[test]
 fn test_healthcheck() {
     let client = Client::new();
 
-    let res = client.get("/healthcheck").send().unwrap();
+    let res = client.get(format!("{}/healthcheck", URL)).send().unwrap();
 
     assert_eq!(res.status(), StatusCode::OK);
 }
