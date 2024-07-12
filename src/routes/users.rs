a27602d59628c37b5fa88bf33c0bb8eefb25e8cf
Armin <armhamos@gmail.com>
2024-07-12 22:11:20 +0300

    fix: bubble up user errors

--- src/routes/users.rs
+++ src/routes/users.rs
@@ -12,11 +12,19 @@ use crate::{
 
 use crate::{get_conn, AppStateType};
 
-pub async fn all(State(state): AppStateType) -> Json<Value> {
+pub async fn all(State(state): AppStateType) -> (StatusCode, Json<Value>) {
     let mut conn = get_conn(&state.pool).await;
 
-    let users = UsersRepository::find_multiple(&mut conn).await.unwrap();
-    Json(json!({"users": users }))
+    match UsersRepository::find_multiple(&mut conn).await {
+        Ok(users) => (StatusCode::OK, Json(json!({"users": users }))),
+        Err(err) => {
+            tracing::info!("Error: {}", err);
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!({"message": "Error fetching users"})),
+            )
+        }
+    }
 }
 
 pub async fn one(State(state): AppStateType) -> Json<Value> {
@@ -42,8 +50,10 @@ pub async fn update(
     Json(payload): Json<UpdatableFieldsUser>,
 ) -> (StatusCode, Json<Value>) {
     let mut conn = get_conn(&state.pool).await;
-    let updated_user = UsersRepository::update(&mut conn, user_id, payload).await.unwrap();
-    
+    let updated_user = UsersRepository::update(&mut conn, user_id, payload)
+        .await
+        .unwrap();
+
     (StatusCode::OK, Json(json!(updated_user)))
 }
 
